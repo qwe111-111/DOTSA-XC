d034f1816bad2712c3865fd6a83935b687f5f626
zsd2024 <zsd_2024@outlook.com>
2025-07-12 15:21:04 +0800

    Add some config

--- src/DOTSAXC/DOTSAXCMod.java
+++ src/DOTSAXC/DOTSAXCMod.java
@@ -1,5 +1,7 @@
 package DOTSAXC;
 
+import java.util.Locale.Category;
+
 import mindustry.mod.Mod;
 
 public class DOTSAXCMod extends Mod {
@@ -12,6 +14,10 @@ public class DOTSAXCMod extends Mod {
     public void loadContent() {
         mlr = new MuiltiLiquidRouter("液体传输器") {
             {
+                requirements(Category.liquid, with());
+                liquidCapacity = 700f;
+                size = 2;
+                solid = true;
             }
         };
     }
